bc74a4b222a990197c2ad898b74b57019aed230f
bethjm <b.jeanne.mills@gmail.com>
2024-01-11 14:47:58 -0500

    configured settngs.py

--- recipe_project/urls.py
+++ recipe_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for recipe_algo project.
+URL configuration for recipe_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
