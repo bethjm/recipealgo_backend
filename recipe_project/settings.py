deb2ac90b52f12653e815adf2fbff9b5d111b3d5
bethjm <b.jeanne.mills@gmail.com>
2024-01-11 20:02:16 -0500

    .

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zwb48cxrzqt!o1zi$i-0j6250%enkf%bg-(d^+h7*p_ylvga6=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
