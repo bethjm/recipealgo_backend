ee9771895b45fecaf0f2f1bfbee20bd655a8ae65
bethjm <b.jeanne.mills@gmail.com>
2024-01-11 20:12:11 -0500

    urls.py change

--- recipe_project/urls.py
+++ recipe_project/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('recipe_app.urls')),  # Include the URLs from your app
+    path('api/', include('recipe_app.urls')),  # Include the URLs from your app
     # Add any other top-level URL patterns if needed
 ]
