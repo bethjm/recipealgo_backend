0361bd6c8301d1acdbc67068c340b65f2fea5995
bethjm <b.jeanne.mills@gmail.com>
2024-01-11 19:28:04 -0500

    heroku update

--- recipe_project/wsgi.py
+++ recipe_project/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys  # Add this line
+
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipe_project.settings')
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 application = get_wsgi_application()
